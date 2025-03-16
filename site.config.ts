d06e52b0169205d8e4f19d3d70797c13f64953d4
Steve Walson <75564431+stevewalson@users.noreply.github.com>
2025-03-16 03:58:41 +0100

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,17 +9,16 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Steve Blog',
+  domain: 'walson.dev',
+  author: 'Steve Walson',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Me testing this',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'steve_walson',
+  github: 'stevewalson',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
